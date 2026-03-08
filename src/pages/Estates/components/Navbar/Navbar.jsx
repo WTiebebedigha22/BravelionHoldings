540a1c633892a19d8c1463f85485d22656bd45f1
Ware Tiebebedigha <106900112+WTiebebedigha22@users.noreply.github.com>
2026-03-08 22:46:37 +0100

    00.00.1

--- src/pages/Estates/components/Navbar/Navbar.jsx
+++ src/pages/Estates/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Mail, Phone, Menu, X, ArrowRight } from 'lucide-react';
-import "./navbar.css";
+import "./Navbar.css";
 
 const EstatesNavbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
